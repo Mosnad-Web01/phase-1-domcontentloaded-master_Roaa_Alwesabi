15d38c0f43eb1d1a0fdec8fc5cfe20a2b77229bb
Roaa2002-web <roaa.abdulfattah.alwesabi@gmail.com>
2024-08-17 17:27:55 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // الحصول على عنصر الفقرة بواسطة id
+    var paragraph = document.getElementById("text");
+    // تغيير النص باستخدام textContent
+    paragraph.textContent = "This is really cool!";
+  });
+  
